0c35b74e860fb808818a5caddac886cf7bd90b14
abdallah-mohamed <P@ssw0rd>
2023-01-15 14:09:51 +0200

    writing docker compose and restructuring

---
+++ database/create_events.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS events
+(
+    event_date date,
+    event_time time without time zone,
+    event_location text ,
+    event_title text ,
+    event_artists text[] ,
+    event_works text[] ,
+    image_link text 
+)
